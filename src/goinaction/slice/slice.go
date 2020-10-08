9a06757dcb8dac0b8b2a06dd10046b2164659e79
Gabriel Lee <gabrielleeyj@outlook.com>
2020-10-08 16:23:39 +0800

    slice example

---
+++ src/goinaction/slice/slice.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("template.gohtml"))
+}
+func main() {
+
+	modules := []string{"Go Basic", "Go Advanced", "Go In Action", "Go Microservices"}
+
+	err := tpl.Execute(os.Stdout, modules)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
