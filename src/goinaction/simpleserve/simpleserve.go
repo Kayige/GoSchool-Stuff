ddd60a5cdad36d9a487c497299d74b2dc7d7ff5a
Gabriel Lee <gabrielleeyj@outlook.com>
2020-10-08 10:28:56 +0800

    simple http serve method

---
+++ src/goinaction/simpleserve/simpleserve.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//ServeHTTPPage tagged to a type int
+type ServeHTTPPage int
+
+func (m ServeHTTPPage) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("GoSchoolKey", "This is from GoSchool")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h1>Your code in this func will run</h1>")
+}
+func main() {
+	var d ServeHTTPPage
+	http.ListenAndServe(":8080", d)
+}
