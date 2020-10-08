babbe4e9ffdc3a6b217599781cdc3f8624c74751
Gabriel Lee <gabrielleeyj@outlook.com>
2020-10-08 16:22:31 +0800

    global function

---
+++ src/goinaction/globalfunction/globalfunction.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("template.gohtml"))
+}
+func main() {
+	g1 := struct {
+		Value1 int
+		Value2 int
+	}{
+		7,
+		9,
+	}
+	err := tpl.Execute(os.Stdout, g1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
